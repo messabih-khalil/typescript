652660cccb04a89c455214eac547ae5925381f45
aladin <msbih.khalil@gmail.com>
2022-10-09 18:54:13 +0100

    typeAnnotations

---
+++ typeAnnotations.ts
@@ -0,0 +1,13 @@
+
+// Type Annotations With Object
+
+
+let userObject : {
+    id : number,
+    name : string,
+    books : Array<string>
+} = {
+    id : 15,
+    name : "alaa",
+    books : ["api handbook" , "linux for dumies"]
+}
